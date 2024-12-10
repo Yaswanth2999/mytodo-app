d154170d3c0e189df3c91e5157ded93603de034c
Yaswanth <Singuluri.Kumar@coforge.com>
2024-12-10 11:57:35 +0530

    Updates

---
+++ precache-manifest.ac83977c0b517467698b6e11f21e82f0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8937b6243892c69a714161091011ee4b",
+    "url": "/Yaswanth2999/mytodo-app/index.html"
+  },
+  {
+    "revision": "8d10777edc2d298e4173",
+    "url": "/Yaswanth2999/mytodo-app/static/css/main.235cdd81.chunk.css"
+  },
+  {
+    "revision": "1ba7e2e8735611353a31",
+    "url": "/Yaswanth2999/mytodo-app/static/js/2.1f583f33.chunk.js"
+  },
+  {
+    "revision": "7fd0ee5d656fc2a49466450e3a87d220",
+    "url": "/Yaswanth2999/mytodo-app/static/js/2.1f583f33.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e197d50f0c521808a666",
+    "url": "/Yaswanth2999/mytodo-app/static/js/3.14750dfe.chunk.js"
+  },
+  {
+    "revision": "8d10777edc2d298e4173",
+    "url": "/Yaswanth2999/mytodo-app/static/js/main.357d6918.chunk.js"
+  },
+  {
+    "revision": "8091cc335a5f5dd7e4bd",
+    "url": "/Yaswanth2999/mytodo-app/static/js/runtime-main.a6a584c8.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/Yaswanth2999/mytodo-app/static/media/logo.06e73328.svg"
+  }
+]);
